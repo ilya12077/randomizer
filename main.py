4df78c6960161a549147e8de3b83b360c39c63e6
ilya12077 <mr.ilya.1207@gmail.com>
2024-11-07 01:41:04 +0300

    MVP

---
+++ main.py
@@ -0,0 +1,119 @@
+import os
+import random
+import time
+
+import requests
+from flask import Flask, request
+from waitress import serve
+
+import tools
+
+app = Flask(__name__)
+
+if os.environ.get('AM_I_IN_A_DOCKER_CONTAINER', False):
+    path = '/etc/randomizer/'
+else:
+    path = ''
+
+pendingupdates_lastchecked = 0
+pendingupdates_lastsent = 0
+ping = 0
+
+with open(f'{path}data/userids.txt', 'r', encoding='utf-8') as f:
+    gifted_userids = f.read().split()
+with open(f'{path}data/prizes.txt', 'r', encoding='utf-8') as f:
+    prizes = f.readlines()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def firewall():
+    global pendingupdates_lastchecked, pendingupdates_lastsent, ping
+    if request.method == "GET":
+        return 'I\'m working'
+    r = request.get_json()
+    with open(f'{path}data/quires.txt', 'a', encoding='utf-8') as f:
+        f.write(str(r) + '\n')
+    print(r)
+    current_time = time.time()
+    if current_time - pendingupdates_lastchecked > 60:
+        pendingupdates_lastchecked = current_time
+        response = requests.get(f'{tools.url}getWebhookInfo')
+        if response.status_code == 200:
+            pendingupdates_count = response.json().get("result", {}).get("pending_update_count", 0)
+            if pendingupdates_count > 25:
+                if current_time - pendingupdates_lastsent > 60 * 5:  # 3600 секунд = 1 час
+                    tools.send_message(647372660, f'⭕Я заметил, что pending updates сейчас: <b>{pendingupdates_count}</b>\n{tools.url}getWebhookInfo')
+                    pendingupdates_lastsent = current_time
+    if 'callback_query' in r:
+        chat_id = str(r['callback_query']['message']['chat']['id'])
+        if chat_id == r['callback_query']['data']:
+            user_id = str(r['callback_query']['from']['id'])
+            if user_id not in gifted_userids:
+                if not prizes:
+                    tools.send_message(user_id, 'К сожалению, все призы закончились.')
+                else:
+                    chosen_prize = random.choice(prizes)
+                    prizes.remove(chosen_prize)
+                    tools.send_message(chat_id, f'вот твой приз поздравляю: {chosen_prize}')
+                    gifted_userids.append(user_id)
+                    with open(f'{path}data/userids.txt', 'w', encoding='utf-8') as f:
+                        f.write(' '.join(gifted_userids))
+                    with open(f'{path}data/prizes.txt', 'w', encoding='utf-8') as f:
+                        f.write(''.join(prizes))
+                    tools.delete_message(chat_id, r['callback_query']['message']['message_id'])
+            else:
+                tools.send_message(chat_id, 'Вы уже получили свой приз. Дайте шанс остальным!')
+        requests.post(tools.url + f"answerCallbackQuery?callback_query_id={r['callback_query']['id']}")
+        return 'OK'
+    if 'message' in r:
+        ping = round(current_time - int(r['message']['date']), 2)
+        print(f'ping: {ping}s. ')
+        if r['message']['chat']['type'] == 'private':
+            dm_handler(r)
+    return 'OK'
+
+
+def dm_handler(r):
+    user_id = str(r['message']['from']['id'])
+    chat_id = r['message']['chat']['id']
+    if 'text' in r['message']:
+        msg = r['message']['text']
+        match msg:
+            case '/start':
+                if user_id not in gifted_userids:
+                    tools.send_message(user_id, 'Добро пожаловать! вижу тебя в первый раз', {'inline_keyboard': [[{'text': 'Попытать удачу', 'callback_data': chat_id}]]})
+                else:
+                    tools.send_message(chat_id, 'Вы уже получили свой приз. Дайте шанс остальным!')
+            case '/logs' if user_id == '647372660':
+                with open(f'{path}data/log.txt', 'r', encoding='utf-8') as f:
+                    log = []
+                    for line in f:
+                        index = line.find('{')
+                        if index != -1:
+                            log.append(line[:index] + '\n')
+                        else:
+                            log.append(line)
+                    log = ''.join(log)
+                    if len(log) <= 4096:
+                        send_body = {
+                            'chat_id': user_id,
+                            'text': log,
+                            'parse_mode': 'HTML'
+                        }
+                    else:
+                        send_body = {
+                            'chat_id': user_id,
+                            'text': log[-4096:],
+                            'parse_mode': 'HTML'
+                        }
+                    requests.post(tools.url + 'sendMessage', json=send_body)
+            case _:
+                tools.send_message(user_id, 'Неизвестная команда')
+
+
+if __name__ == '__main__':
+    if os.environ.get('AM_I_IN_A_DOCKER_CONTAINER', False):
+        serve(app, host='0.0.0.0', port=8881, url_scheme='http')
+    else:
+        app.run(host='192.168.1.10', port=8890)
+        # app.run(host='192.168.1.27', port=8889)
---
+++ tools.py
@@ -0,0 +1,56 @@
+import datetime
+import os
+
+import pytz
+import requests
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+url = os.environ.get('URL')
+if os.environ.get('AM_I_IN_A_DOCKER_CONTAINER', False):
+    path = '/etc/randomizer/'
+else:
+    path = ''
+
+for filename in ['log.txt', 'quires.txt', 'userids.txt']:
+    if not os.path.isfile(f'{path}data/{filename}'):
+        # Создаем файл, если он не существует
+        with open(f'{path}data/{filename}', 'w', encoding='utf-8') as fl:
+            fl.write('1')
+
+
+def send_message(chat_id: int | str, message, keyboard=None) -> bool:
+    # print(switch_safe_mode, switch_authorize_all, switch_entire_authorization, switch_message_deletion)
+    if keyboard is None:
+        send_body = {
+            'chat_id': chat_id,
+            'text': message,
+            'parse_mode': 'HTML'
+        }
+    else:
+        send_body = {
+            'chat_id': chat_id,
+            'text': message,
+            'parse_mode': 'HTML',
+            'reply_markup': keyboard
+        }
+    r = requests.post(url + 'sendMessage', json=send_body)
+    return True if r.json()['ok'] else False
+
+
+def delete_message(chat_id, message_id) -> None:
+    requests.post(url + f'deleteMessage?chat_id={chat_id}&message_id={message_id}')
+
+
+def append_log(msg, ping: int = None) -> None:
+    try:
+        with open(f'{path}data/log.txt', 'a', encoding='utf-8') as f:
+            if not ping:
+                f.write(f'[{datetime.datetime.now(pytz.timezone("Europe/Moscow")).strftime("%H:%M:%S")}]: {msg}' + '\n')
+            else:
+                f.write(f'[{datetime.datetime.now(pytz.timezone("Europe/Moscow")).strftime("%H:%M:%S")}]<b>({ping}s.)</b> : {msg}' + '\n')
+        print(msg)
+    except Exception as e:
+        with open(f'{path}data/log.txt', 'a', encoding='cp1251') as f:
+            f.write(f'[{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}]: {msg}' + '\n')
+            f.write(f'^^^^^caught exception {e}' + '\n')
