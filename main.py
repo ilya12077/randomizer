cf2db6dbf6b00874419cf81d6def2bb54d088599
mrily <mr.ilya.1207@gmail.com>
2024-11-10 18:52:06 +0300

    test

--- main.py
+++ main.py
@@ -22,11 +22,15 @@ ping = 0
 
 with open(f'{path}data/userids.txt', 'r', encoding='utf-8') as f:
     gifted_userids = f.read().split()
-with open(f'{path}data/test.json', 'r', encoding='utf-8') as f:
+with open(f'{path}data/prizes.json', 'r', encoding='utf-8') as f:
     prizes = json.load(f)
 
-weights = [prizes['40']['odds'], prizes['50']['odds'], prizes['80']['odds']]
+weights = [float(prizes['40']['odds']), float(prizes['50']['odds']), float(prizes['80']['odds'])]
 categories = ['40', '50', '80']
+category_to_index = {'40': 0, '50': 1, '80': 2}
+categories_messages = ['🤑Личная скидка 40% на:\n\nБейсболка "НЛО", Бейсболка "НЛО" black, Кепка "Halloween edition", Лонгслив "Серебро" boiled gray, Лонгслив "Бензин" white, Лонгслив "Серебро" black, Кепка "Пират" gray,  Оверсайз брюки бензин серые, Оверсайз брюки базовые серые, Оверсайз брюки бензин черные, Оверсайз брюки базовые черные.\n\n👽 Скидка на весь каталог 30%\n\n💚 Скидки не суммируются. Забирай выигрыш на сайте: https://invmerch.ru/collection/kosmonavtov-net\n\nакция действует до 2.12.2024',
+                       '🤑 Личная скидка 50% на: \n\nФутболка "Твои глаза" v.1, Футболка "Твои глаза" v.2, Футболка "Космос нас не ждет" gray, Футболка "Космос нас не ждет" black, Футболка КН х 13 карат, Худи "Бензин", Худи "Серебро" chocolate, Худи "Бензин" chocolate, Маска "Бензин"\n\n👽 Скидка на весь каталог 30%\n\n💚 Скидки не суммируются. Забирай выигрыш на сайте: https://invmerch.ru/collection/kosmonavtov-net\n\nакция действует до 2.12.2024',
+                       '🤑 Личная скидка 80% на: \n\nЛонгслив КН х 13 карат, Свитер "Chistmas Vibe" gray\n\n👽 Скидка на весь каталог 30%\n\n💚 Скидки не суммируются. Забирай выигрыш на сайте: https://invmerch.ru/collection/kosmonavtov-net\n\nакция действует до 2.12.2024']
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -53,28 +57,30 @@ def firewall():
         if chat_id == r['callback_query']['data']:
             user_id = str(r['callback_query']['from']['id'])
             first_name = str(r['callback_query']['from']['first_name'])
-            if user_id not in gifted_userids:
+            requests.post(tools.url + f"answerCallbackQuery?callback_query_id={r['callback_query']['id']}")
+            # if user_id not in gifted_userids:
+            if True:
                 available_categories = [cat for cat in categories if prizes[cat]['codes']]
-                available_weights = [weights[categories.index(cat)] for cat in available_categories]
+                available_weights = [float(weights[categories.index(cat)]) for cat in available_categories]
                 # Если нет доступных кодов во всех категориях, возвращаем None
                 if not available_categories:
                     tools.send_message(user_id, 'К сожалению, все призы закончились.')
-                    return None
+                    return 'ok'
                 chosen_category = random.choices(available_categories, weights=available_weights, k=1)[0]
                 if chosen_category in prizes and prizes[chosen_category]['codes']:
                     # Извлекаем и удаляем первый код
                     chosen_code = prizes[chosen_category]['codes'].pop(0)
-                    tools.send_message(chat_id, f'Ты выиграл {chosen_category} {chosen_code}')
+                    tools.send_message(chat_id, f'Поздравляю! Личная скидка для тебя по промокоду <code>{chosen_code}</code>. Сейчас расскажу на что она действует!')
+                    tools.send_message(user_id, categories_messages[category_to_index[chosen_category]])
                     gifted_userids.append(user_id)
                     with open(f'{path}data/userids.txt', 'w', encoding='utf-8') as f:
                         f.write(' '.join(gifted_userids))
-                    with open("prizes.json", "w", encoding='utf-8') as file:
-                        json.dump(prizes, file, indent=2)
+                    with open(f'{path}data/prizes.json', 'w', encoding='utf-8') as f:
+                        json.dump(prizes, f, indent=2)
                     tools.delete_message(chat_id, r['callback_query']['message']['message_id'])
                     tools.append_log(f'Выдан {chosen_category}%: {chosen_code} {first_name}({user_id})')
             else:
                 tools.send_message(chat_id, 'Ты уже получил свой приз. Дай шанс остальным!')
-        requests.post(tools.url + f"answerCallbackQuery?callback_query_id={r['callback_query']['id']}")
         return 'OK'
     if 'message' in r:
         ping = round(current_time - int(r['message']['date']), 2)
@@ -91,7 +97,7 @@ def dm_handler(r):
         msg = r['message']['text']
         match msg:
             case '/start':
-                if user_id not in gifted_userids:
+                if user_id not in gifted_userids or True:
                     tools.send_message(user_id, 'Добро пожаловать! Сегодня у тебя есть уникальная возможность выиграть призы от любимой группы!', {'inline_keyboard': [[{'text': 'Попытать удачу', 'callback_data': chat_id}]]})
                 else:
                     tools.send_message(chat_id, 'Ты уже получил свой приз. Дай шанс остальным!')
@@ -122,9 +128,9 @@ def dm_handler(r):
                 # Проверяем наличие кодов в каждом разделе
                 for key, value in prizes.items():
                     if not value["codes"]:  # Если список кодов пуст
-                        tools.send_message(user_id, f"Коды для {key} закончились.")
+                        tools.send_message(user_id, f"Коды для {key}% закончились.")
                     else:
-                        tools.send_message(user_id, f"Коды для {key} ещё есть ({len(value['codes'])} шт.).")
+                        tools.send_message(user_id, f"Коды для {key}% ещё есть ({len(value['codes'])} шт.).")
             case _:
                 tools.send_message(user_id, 'Неизвестная команда')
 
@@ -133,8 +139,8 @@ if __name__ == '__main__':
     if os.environ.get('AM_I_IN_A_DOCKER_CONTAINER', False):
         serve(app, host='0.0.0.0', port=8881, url_scheme='http')
     else:
-        app.run(host='192.168.1.10', port=8890)
-        # app.run(host='192.168.1.27', port=8889)
+        # app.run(host='192.168.1.10', port=8890)
+        app.run(host='192.168.1.27', port=8890)
 # TODO:
 # нет кодов, категория выше, отдельная функц
 # по категориям хелпа
