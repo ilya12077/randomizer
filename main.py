8b28d64e14f22265d941815596b9dd4744cfc6bc
mrily <mr.ilya.1207@gmail.com>
2025-07-22 21:00:31 +0300

    product

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ def dm_handler(r):
 
 if __name__ == '__main__':
     if os.environ.get('AM_I_IN_A_DOCKER_CONTAINER', False):
-        serve(app, host='0.0.0.0', port=8881, url_scheme='http')
+        serve(app, host='localhost', port=1111, url_scheme='http')
     else:
         app.run(host='192.168.1.10', port=8881)
         # app.run(host='192.168.1.27', port=8890)
